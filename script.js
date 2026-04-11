dea3ab38f05c8c6e47887a0f44ab6842400a036a
petervu2208 <petervu2208@gmail.com>
2026-04-11 15:57:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,134 @@
 const modules = {
   1: [
-    "S3_LINK_IMAGE_1",
-    "S3_LINK_IMAGE_2"
-  ],
-  2: [
-    "S3_LINK_IMAGE_3"
-  ],
-  3: [],
-  4: [],
-  5: [],
-  6: [],
-  7: [],
-  8: [],
-  9: []
-};
+    "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module-1-01.jpg",
+    "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module-1-IAMLab_1.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module1-IAMLab_2.jpg"
+    ],
+    2: [
+    "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module2-01.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module2-02.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module2-EFS_1.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module2-EFS_2.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module2-EFS_3.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module2-EFS_4.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module2-EFS_5.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module2-S3CLI_1.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module2-S3CLI_2.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module2-S3CLI_3.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module2-S3CLI_4.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module2-S3CLI_5.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module2-S3CLI_6.jpg"
+    ],
+    3: [
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module3-01.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module3-02.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module3-EC2_1.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module3-EC2_2.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module3-EC2_3.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module3-EC2_4.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module3-EC2_5.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module3-EC2_6.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module3-EC2_7.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module3-EC2_8.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module3-EC2_9.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module3-EC2CLI_1.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module3-EC2CLI_2.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module3-EC2CLI_3.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module3-EC2CLI_4.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module3-EC2CLI_5.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module3-EC2CLI_6.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module3-EC2CLI_7.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module3-EC2CLI_8.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module3-EC2CLI_9.jpg"
+	],
+    4: [
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module4-01.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module4-02.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module4-ALBviaWeb_1.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module4-ALBviaWeb_2.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module4-ALBviaWeb_3.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module4-ALBviaWeb_4.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module4-ALBviaWeb_5.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module4-ALBviaWeb_6.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module4-ELBviaCLI_1.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module4-ELBviaCLI_2.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module4-ELBviaCLI_3.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module4-ELBviaCLI_4.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module4-ELBviaCLI_5.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module4-ELBviaCLI_6.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module4-ELBviaCLI_7.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module4-ELBviaCLI_8.jpg",
+	],
+    5: [
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module5-01.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module5-EKS_1.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module5-EKS_2.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module5-EKS_3.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module5-EKS_4.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module5-EKS_5.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module5-Kubernetes_1.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module5-Kubernetes_2.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module5-Kubernetes_3.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module5-Kubernetes_4.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module5-Kubernetes_5.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module5-Kubernetes_6.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module5-Kubernetes_7.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module5-Kubernetes_8.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module5-Kubernetes_9.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module5-Kubernetes_10.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module5-Kubernetes_11.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module5-Kubernetes_12.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module5-Kubernetes_13.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module5-Kubernetes_14.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module5-Kubernetes_15.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module5-Kubernetes_16.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module5-Kubernetes_17.jpg"
+	],
+    6: [
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module6-01.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module6-02.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module6-SNSSQS_1.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module6-SNSSQS_2.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module6-SNSSQS_3.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module6-SNSSQS_4.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module6-SNSSQS_5.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module6-SNSSQS_6.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module6-SNSSQS_7.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module6-SNSSQS_8.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module6-SNSSQS_9.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module6-SNSSQS_10.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module6-SNSSQS_11.jpg"
+	],
+    7: [
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module7-01.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module7-DynamoDB_1.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module7-DynamoDB_2.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module7-DynamoDB_3.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module7-DynamoDB_4.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module7-DynamoDB_5.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module7-DynamoDB_6.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module7-DynamoDB_7.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module7-DynamoDB_8.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module7-DynamoDB_9.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module7-DynamoDB_10.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module7-DynamoDB_11.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module7-Lambda_1.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module7-Lambda_2.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module7-Lambda_3.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module7-Lambda_4.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module7-Lambda_5.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module7-Lambda_6.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module7-Lambda_7.jpg"
+	],
+    8: [
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module8-01.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module8-02.jpg",
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module8-03.jpg"
+	],
+    9: [
+	  "https://cs79c-portfolio.s3.us-east-1.amazonaws.com/module9-01.jpg"
+	]
+  };
 
 function openModule(moduleNumber) {
   document.getElementById("moduleView").style.display = "none";
